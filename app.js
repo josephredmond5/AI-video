e8456f5c1785dd13fd7e46d57e9b7d800670dfc6
josephredmond5 <josephredmond5@icloud.com>
2023-07-23 12:46:04 +0100

    added the basics to the html file with a form and added the styles

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const { generateMeta, generateImage } = require('./controllers/OpenaiController'
 
 // app setup
 const app = express()
-app.listen(4000, () =>  console.log('listening for requests on port 4000'))
+app.listen(4001, () =>  console.log('listening for requests on port 4001'))
 
 // middleware
 app.use(express.json())
+app.use(express.static('public')) //  sets up a static file server in the Express.js application
 
 // routes
 app.post('/openai/meta', generateMeta)
